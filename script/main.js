325561ad6e54a6cb7a1549994d1ff95997fbdf8a
Duc Thang Vu <vuducthang91@gmail.com>
2020-04-03 16:51:57 +0200

    commit other snack

--- script/main.js
+++ script/main.js
@@ -1,3 +1,63 @@
+/************/
+/* A script for solving certain problems as defined in README.md
+
+/************/
+
+
+/* 1 
+L’utente inserisce due parole in successione, con due prompt. Il software stampa la parola più lunga. Utilizza una funzione per determinare e ritornare la parola più lunga. */
+
+var word_1 = prompt('Enter first word')
+var word_2 = prompt('Enter second word')
+
+function longest_word_among_two(word_1, word_2) {
+    // A function accepting two strings, and returning the longest or 0 if they are equally long; provided that empty spaces on both side of both strings are ignored. Return -1 if either parameter is not a string.
+    if (typeof(word_1) != 'string' || typeof(word_2) != 'string' ) {return -1}   // validation
+
+    if (word_1.trim().length > word_2.trim().length) {
+        return word_1.trim()
+    } else if (word_1.trim().length == word_2.trim().length) {
+        return 0
+    } else {
+        return word_2.trim()
+    }
+}
+
+
+
+
+/* 2 
+Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo
+var number = prompt('Enter a number:') */
+
+function ifOdd_plus1(number) {
+    // A function accepting a number, and returning the same number if even, otherwise odd.
+    if (number % 2) {
+        return number
+    } else {
+        return ++number
+    }
+}
+
+
+
+/* 3 
+Generatore di “nomi cognomi” casuali: prendendo una lista di nomi (‘Michele’, ‘Fabio’, ‘Roberto’) e una lista di cognomi (‘Forghieri’, ‘Papagni’, ‘Marazzini’), Gatsby vuole generare una falsa lista di 5 invitati. */
+
+var names = ['Michele', 'Fabio', 'Roberto']
+var surnames = ['Forghieri', 'Papagni', 'Marazzini']
+
+
+function randomNames(names, surnames) {
+    // a function accepting two arrays of names and surnames, and returning an array of five strings, each combining randomly one name and one surname
+    var invited = []
+    
+    for (i = 5; i < 6; i++) {
+        invited.push(names[Math.floor(Math.random(names.length))] + ' ' + surnames[Math.floor(Math.random(surnames.length))])
+    }
+
+    return invited
+}
 
 
 /* 4
