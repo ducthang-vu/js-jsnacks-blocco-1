1671f4061fd9c2eba9cc6352258906397b8e5cab
Duc Thang Vu <vuducthang91@gmail.com>
2020-04-03 17:35:05 +0200

    fixing

--- script/main.js
+++ script/main.js
@@ -74,10 +74,8 @@ function problem3() {
 
 /* 4
 Crea un array di numeri interi, ad esempio (1, 4, 2, 15, 120, 17, 3) e fai la somma di tutti gli elementi che sono in posizione dispari. Prova prima senza funzione e poi con funzione. */
-
-
-
 function sumIndexOdd_inArray(array) {
+    // A function accepting an array, and returning the sum of all elemento of odd index position. 
     var sum = 0
     for (i = 1; i < array.length; i += 2) {
         sum += array[i]
