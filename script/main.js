b2b5d5048259a6e8e0f20efb0d683f183f2c3f04
Duc Thang Vu <vuducthang91@gmail.com>
2020-04-03 17:18:55 +0200

    html and css

--- script/main.js
+++ script/main.js
@@ -7,9 +7,6 @@
 /* 1 
 L’utente inserisce due parole in successione, con due prompt. Il software stampa la parola più lunga. Utilizza una funzione per determinare e ritornare la parola più lunga. */
 
-var word_1 = prompt('Enter first word')
-var word_2 = prompt('Enter second word')
-
 function longest_word_among_two(word_1, word_2) {
     // A function accepting two strings, and returning the longest or 0 if they are equally long; provided that empty spaces on both side of both strings are ignored. Return -1 if either parameter is not a string.
     if (typeof(word_1) != 'string' || typeof(word_2) != 'string' ) {return -1}   // validation
@@ -23,7 +20,12 @@ function longest_word_among_two(word_1, word_2) {
     }
 }
 
+function problem1() {}
+    var word_1 = prompt('Enter first word')
+    var word_2 = prompt('Enter second word')
 
+    console.log(longest_word_among_two(word_1, word_2))
+}
 
 
 /* 2 
@@ -39,6 +41,10 @@ function ifOdd_plus1(number) {
     }
 }
 
+function problem2() {
+    var number = prompt('Enter first word')
+    console.log(ifOdd_plus1(number))
+}
 
 
 /* 3 
@@ -63,7 +69,7 @@ function randomNames(names, surnames) {
 /* 4
 Crea un array di numeri interi, ad esempio (1, 4, 2, 15, 120, 17, 3) e fai la somma di tutti gli elementi che sono in posizione dispari. Prova prima senza funzione e poi con funzione. */
 
-var array = [1, 4, 2, 15, 120, 17, 3]
+
 
 function sumIndexOdd_inArray(array) {
     var sum = 0
@@ -74,4 +80,26 @@ function sumIndexOdd_inArray(array) {
     return sum
 }
 
-console.log(sumIndexOdd_inArray(array))
+
+function problem4() {
+    var array = [1, 4, 2, 15, 120, 17, 3]
+    console.table(array)
+    console.log(sumIndexOdd_inArray(array))
+}
+
+
+/****************/
+/* MAIN SCRIPT */
+/****************/
+
+var button1 = document.getElementById('button1')
+var button2 = document.getElementById('button2')
+var button3 = document.getElementById('button3')
+var button4 = document.getElementById('button4')
+
+
+// EVENTS
+button1.addEventListener('click', problem1())
+button2.addEventListener('click', problem2())
+button3.addEventListener('click', problem3())
+button4.addEventListener('click', problem4())
